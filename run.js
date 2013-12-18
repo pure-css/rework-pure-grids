9fa875c29634d9436f1f060171f48e93f335cc15
Tilo Mitra <tilomitra@gmail.com>
2013-12-18 14:36:55 -0500

    add run.js: run this to see responsive grids get generated

---
+++ run.js
@@ -0,0 +1,26 @@
+var rework = require('rework');
+var fs = require('fs');
+var pureGrids = require('./');
+var gridCss = rework('').use(pureGrids.units(12))
+                        .use(pureGrids.mediaQueries([
+                            {
+                                minWidth: '480px',
+                                gridName: 'med'
+                            },
+                            {
+                                minWidth: '980px',
+                                gridName: 'lrg'
+                            },
+                            {
+                                minWidth: '1200px',
+                                gridName: 'xlrg'
+                            }
+                        ])).toString();
+
+fs.writeFile("grid.css", gridCss, function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("grid.css was saved!");
+    }
+});
