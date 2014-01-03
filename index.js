169f7b466e00426c04fba8b2eed3dd3004d11e37
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-01-02 22:52:33 -0500

    Tweak whitespace in comment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*
- Copyright (c) 2013, Yahoo! Inc. All rights reserved.
- Copyrights licensed under the New BSD License.
- See the accompanying LICENSE file for terms.
- */
+Copyright (c) 2013, Yahoo! Inc. All rights reserved.
+Copyrights licensed under the New BSD License.
+See the accompanying LICENSE file for terms.
+*/
 
 'use strict';
 
